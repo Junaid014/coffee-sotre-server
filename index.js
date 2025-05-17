fb5d50e5f67f96e0b400226839ab19cbe90a3268
junaid014 <junaidhossen014@gmail.com>
2025-05-17 15:25:24 +0600

    add update method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app =express();
@@ -26,7 +26,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+   
+    const coffeeCollection=client.db('coffeeDB2').collection('coffees')
+
+    app.get('/coffees',async(req,res)=>{
+      const result=await coffeeCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/coffees/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query ={ _id: new ObjectId(id)}
+      const result=await coffeeCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.post('/coffees',async(req,res)=>{
+      const newCoffee =req.body;
+      console.log(newCoffee);
+      const result=await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+    })
+
+    app.put('/coffees/:id' ,async(req,res)=>{
+      const id=req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options ={upsert:true};
+      const updatedCoffee=req.body;
+      const updatedDoc ={
+        $set: updatedCoffee
+      }
+      const result =await coffeeCollection.updateOne(filter,updatedDoc,options)
+      res.send(result)
+    })
+
+    app.delete('/coffees/:id' , async(req,res)=>{
+      const id =req.params.id;
+      const query={_id : new ObjectId(id)}
+      const result=await coffeeCollection.deleteOne(query);
+      res.send(result)
+    })
    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
