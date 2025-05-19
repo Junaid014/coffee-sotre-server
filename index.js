39d86e92a0e13a0278cfc111bfa7b3e7f9dafbcb
junaid014 <junaidhossen014@gmail.com>
2025-05-19 14:58:16 +0600

    add changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
-const app =express();
-const port =process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
@@ -27,75 +27,82 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-  //  All collection............
-    const coffeeCollection=client.db('coffeeDB2').collection('coffees')
-    const usersCollection=client.db('coffeeDB2').collection('users')
+    //  All collection............
+    const coffeeCollection = client.db('coffeeDB2').collection('coffees')
+    const usersCollection = client.db('coffeeDB2').collection('users')
 
-    app.get('/coffees',async(req,res)=>{
-      const result=await coffeeCollection.find().toArray();
+    app.get('/coffees', async (req, res) => {
+      const result = await coffeeCollection.find().toArray();
       res.send(result)
     })
 
-    app.get('/coffees/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query ={ _id: new ObjectId(id)}
-      const result=await coffeeCollection.findOne(query);
+    app.get('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.findOne(query);
       res.send(result)
     })
 
-    app.post('/coffees',async(req,res)=>{
-      const newCoffee =req.body;
+    // for my coffee section to get the specific coffees which a user upload 
+    app.get('/myCart/:email', async (req, res) => {
+
+      const result = await coffeeCollection.find({ email: req.params.email }).toArray();
+      res.send(result)
+    })
+
+    app.post('/coffees', async (req, res) => {
+      const newCoffee = req.body;
       console.log(newCoffee);
-      const result=await coffeeCollection.insertOne(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result)
     })
 
-    app.put('/coffees/:id' ,async(req,res)=>{
-      const id=req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const options ={upsert:true};
-      const updatedCoffee=req.body;
-      const updatedDoc ={
+    app.put('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const updatedDoc = {
         $set: updatedCoffee
       }
-      const result =await coffeeCollection.updateOne(filter,updatedDoc,options)
+      const result = await coffeeCollection.updateOne(filter, updatedDoc, options)
       res.send(result)
     })
 
-    app.delete('/coffees/:id' , async(req,res)=>{
-      const id =req.params.id;
-      const query={_id : new ObjectId(id)}
-      const result=await coffeeCollection.deleteOne(query);
+    app.delete('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result)
     })
 
 
     // User related APIS
 
-    app.get('/users' , async(req,res)=>{
-      const result= await usersCollection.find().toArray();
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result)
     })
 
-    app.post('/users' ,async(req,res)=>{
-      const userProfile =req.body;
+    app.post('/users', async (req, res) => {
+      const userProfile = req.body;
       console.log(userProfile);
-      const result =await usersCollection.insertOne(userProfile);
+      const result = await usersCollection.insertOne(userProfile);
       res.send(result)
     })
-   
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send("coffee server")
+app.get('/', (req, res) => {
+  res.send("coffee server")
 });
-app.listen(port,()=>{
-    console.log(`coffee server is running  port ${port}`);
+app.listen(port, () => {
+  console.log(`coffee server is running  port ${port}`);
 })
